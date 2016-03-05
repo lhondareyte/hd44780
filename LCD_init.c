5ed780e2daeb96690e46cdea80f3fa88b7449ba8
Luc Hondareyte <luc.hondareyte@laposte.net>
2016-03-05 14:57:21 +0100

    Remove RCS tags

--- LCD_init.c
+++ LCD_init.c
@@ -1,66 +1,68 @@
  /*
-  * Copyright (c) 2006 Luc HONDAREYTE
+  * Copyright (c) 2006-2012 Luc HONDAREYTE
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions
-  * are met:
-  * 1. Redistributions of source code must retain the above copyright
-  *    notice, this list of conditions and the following disclaimer.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  *  $Id: LCD_init.c,v 0.9 2006/09/16 15:03:30 luc Exp luc $
   */
 
-#ifndef H44780_H
- #include "H44780.h"
-#endif
-
-#include <stdlib.h>
-#include <avr/io.h>
-#include <stdint.h>
+#include "hd44780.h"
 
 void LCD_init (void)
 {
 
-        _H44780_DATA_REG_ = _H44780_DATA_MASK_;
-        _H44780_CLOCK_REG_ |= (1<< H44780_CLOCK_PIN);
+	/* Initialisaion des ports */
+	
+	#if H44780_DATA_WIDTH == 8
+        _H44780_DATA_REG_ =  0xff ;
+
+	#else
+ 	  #if H44780_PORT_IS_LSB == 1
+          _H44780_DATA_REG_ |= 0x0f ;
+	  #else
+          _H44780_DATA_REG_ |= 0xf0 ;
+	  #endif
+	#endif
+
  	_H44780_RS_REG_ |= (1<< H44780_RS_PIN);
-        _H44780_DATA_PORT_ = 0x00;
-        _delay_ms (15);
-        
-#if H44780_PORT_IS_LSB  == 1 
- 
-        _H44780_DATA_PORT_ = 0x03;
- #else
-        _H44780_DATA_PORT_ = 0x30;
- 
+        _H44780_ENABLE_REG_ |= (1<< H44780_ENABLE_PIN);
+
+        _delay_ms(42);
+
+	/* Debut d'initialisation HD44780 */
+
+	WriteNibble(_H44780_DATA_PORT_, 0x03);
+        _H44780_ENABLE_PORT_ &= ~(1<< H44780_ENABLE_PIN);
+        _H44780_RS_PORT_ &= ~(1<< H44780_RS_PIN);
+
+	LCD_validate();
+
+        _delay_ms(4);
+	LCD_validate();
+	LCD_validate();
+
+#if ( H44780_DATA_WIDTH == 8 )
+        LCD_ioctl( 0x30 + H44780_LINES_ARG );
+	LCD_validate();
+#else
+
+	/* Envoi du mode 4bits */
+	WriteNibble(_H44780_DATA_PORT_, 0x02);
+        _H44780_ENABLE_PORT_ &= ~(1<< H44780_ENABLE_PIN);
+        _H44780_RS_PORT_ &= ~(1<< H44780_RS_PIN);
+
+	LCD_validate();
+	LCD_validate();
+
+	/* Envoi du nb de lignes */
+	WriteNibble(_H44780_DATA_PORT_, H44780_LINES_ARG);
+        _H44780_ENABLE_PORT_ &= ~(1<< H44780_ENABLE_PIN);
+        _H44780_RS_PORT_ &= ~(1<< H44780_RS_PIN);
+
+	LCD_validate();
 #endif
-        LCD_enable_ms(5);
-        LCD_enable_us(150);
-
-#if H44780_DATA_WIDTH == 8
-
-        LCD_sendCommand( 0x30 + H44780_LINES_OFFSET );
- #else
-        LCD_sendCommand( 0x20 + H44780_LINES_OFFSET );
-
-#endif
-        LCD_enable_us(400);
-        LCD_sendCommand (H44780_DISPLAY_ON);
-        LCD_sendCommand (H44780_CURSOR_HOME);
-        LCD_sendCommand (H44780_BLINK_OFF);
-        LCD_sendCommand (H44780_CLEAR_DISPLAY);
+        LCD_ioctl (H44780_DISPLAY_OFF);
+        LCD_ioctl (H44780_CURSOR_OFF);
+        LCD_ioctl (H44780_CLEAR_DISPLAY);
+        LCD_ioctl (H44780_CURSOR_RIGHT);
+        LCD_ioctl (H44780_CURSOR_HOME);
 
 }
--- LCD_ioctl.c
+++ LCD_ioctl.c
@@ -1,58 +1,32 @@
  /*
-  * Copyright (c) 2006 Luc HONDAREYTE
+  * Copyright (c) 2006-2012 Luc HONDAREYTE
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions
-  * are met:
-  * 1. Redistributions of source code must retain the above copyright
-  *    notice, this list of conditions and the following disclaimer.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  *  $Id$
   */
 
-#ifndef H44780_H
- #include "H44780.h"
+
+#ifndef __HD44780_H__
+ #include "hd44780.h"
 #endif
 
-#include <stdlib.h>
-#include <avr/io.h>
-#include <stdint.h>
-
-
-void LCD_sendCommand (uint8_t command)
+void LCD_ioctl (uint8_t c)
 {
-        _H44780_DATA_PORT_ = command;
-        _H44780_DATA_REG_ = _H44780_DATA_MASK_;
-
 #if H44780_DATA_WIDTH == 4 
-
-  #if H44780_PORT_IS_LSB  == 1
-        _H44780_DATA_PORT_ = _H44780_DATA_PORT_ >> 4;
-        LCD_enable_us(200);
-        LCD_wait_ms(2);
-        _H44780_DATA_PORT_ = command;
-  #else
-        LCD_enable_us(200);
-        LCD_wait_ms(2);
-        _H44780_DATA_PORT_ = _H44780_DATA_PORT_ << 4;
-  #endif
-
+	char t;
+	t = c >> 4;
+	_H44780_ENABLE_PORT_ &= ~(1<< H44780_ENABLE_PIN);
+	WriteNibble(_H44780_DATA_PORT_,t);
+	_H44780_RS_PORT_ &= ~(1<< H44780_RS_PIN);
+        LCD_validate();
+	_H44780_ENABLE_PORT_ &= ~(1<< H44780_ENABLE_PIN);
+	c &= 0x0f;
+	WriteNibble(_H44780_DATA_PORT_,c);
+	_H44780_RS_PORT_ &= ~(1<< H44780_RS_PIN);
+        LCD_validate();
+#else
+	_H44780_RS_PORT_ |= (1<< H44780_RS_PIN);
+        _H44780_DATA_PORT_ = t;
+        LCD_validate();
 #endif
-        LCD_enable_us(200);
-        LCD_wait_ms(2);
-        _H44780_DATA_PORT_ = 0x00;
-
+	LCD_wait();
 }
