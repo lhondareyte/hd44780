9f71a0f12bd0112566c8d2ea2c79ceb185393c63
Luc Hondareyte <luc.hondareyte@laposte.net>
2021-12-09 21:08:52 +0100

    Update hd44780.mk

--- hd44780.mk
+++ hd44780.mk
@@ -1,7 +1,7 @@
 HD44780_SOURCES = hd44780/LCD_clrline.c hd44780/LCD_gotoxy.c hd44780/LCD_init.c
 HD44780_SOURCES += hd44780/LCD_ioctl.c hd44780/LCD_nputs.c hd44780/LCD_putc.c
-HD44780_SOURCES += hd44780/LCD_puts.c hd44780/LCD_validate.c hd44780/LCD_wait.c
+HD44780_SOURCES += hd44780/LCD_puts.c hd44780/LCD_validate.c
 
 HD44780_OBJECTS = hd44780/LCD_clrline.o hd44780/LCD_gotoxy.o hd44780/LCD_init.o
 HD44780_OBJECTS += hd44780/LCD_ioctl.o hd44780/LCD_nputs.o hd44780/LCD_putc.o
-HD44780_OBJECTS += hd44780/LCD_puts.o hd44780/LCD_validate.o hd44780/LCD_wait.o
+HD44780_OBJECTS += hd44780/LCD_puts.o hd44780/LCD_validate.o
