e9e297f686f81f7216e482a49765d2023b1b9006
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2021-12-18 14:33:36 +0100

    Fix indentation

--- LCD_puts.c
+++ LCD_puts.c
@@ -27,10 +27,9 @@
 
 void LCD_puts (const char *s)
 {
-  register char c;
-  while ( (c = *s++ ) )
-    {
-	if ( c == 0x0a ) LCD_gotoxy(2,1);
-	else LCD_putc (c);
-    }
+	register char c;
+	while ( (c = *s++ ) ) {
+		if ( c == 0x0a ) LCD_gotoxy(2,1);
+		else LCD_putc (c);
+	}
 }
