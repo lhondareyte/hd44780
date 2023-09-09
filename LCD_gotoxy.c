ebb3273605e2f726e0609ddb6c0d4dc53d0b6c1d
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2023-09-09 17:10:30 +0200

    LCD_gotoxy() : Fix compatibility with LCD-1601

--- LCD_gotoxy.c
+++ LCD_gotoxy.c
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: MIT
  *
- * Copyright (c) 2006-2022 Luc Hondareyte
+ * Copyright (c) 2006-2023 Luc Hondareyte
  *
  */
 
@@ -14,22 +14,24 @@ void LCD_gotoxy (uint8_t x, uint8_t y)
 	if (x > H44780_LINES || y > H44780_ROWS)
 		return;
         uint8_t cur= H44780_DDRAM_L1 - 1;
-	if (x == 1) {
-		cur += y;
-	}
+	cur += y;
+
+#if (H44780_LINES == 1) 
+	if ( y > (H44780_ROWS/2))
+		cur += 49;
+#endif
+#if (H44780_LINES >= 2) 
 	else if (x == 2) {	
-		cur += y;
 		cur += H44780_DDRAM_L2;
 	}
+#endif
 #if (H44780_LINES >= 3) 
 	else if (x == 3) {	
-		cur += y;
 		cur += H44780_DDRAM_L3;
 	}
 #endif
 #if (H44780_LINES >= 4) 
 	else if (x == 4) {	
-		cur += y;
 		cur += H44780_DDRAM_L4;
 	}
 #endif
