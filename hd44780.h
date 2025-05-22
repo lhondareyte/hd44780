cec4126f2e43aa298fb9bb3cceb1297aef06a023
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2025-05-22 19:23:23 +0200

    Update prototype

--- hd44780.h
+++ hd44780.h
@@ -269,7 +269,7 @@ void LCD_clrline(uint8_t);		/* Clear current line */
 void LCD_validate(void);		/* Command validate */
 void LCD_wait(void);			/* Internal delays */
 void LCD_nputs(const char *, uint8_t, uint8_t);
-void LCD_puts_pgm(uint8_t,PGM_P const *);
+void LCD_puts_pgm(uint8_t, char *, PGM_P const *);
 
 #if defined (__BLINK_SUPPORT__)
 void LCD_refresh(void);
