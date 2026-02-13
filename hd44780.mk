86619422a1fb26b187105edc2c413febc55a5eab
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2026-02-13 10:32:51 +0100

    Fix typo

--- hd44780.mk
+++ hd44780.mk
@@ -1,6 +1,6 @@
 HD44780_SOURCES = hd44780/LCD_clrline.c hd44780/LCD_gotoxy.c hd44780/LCD_init.c
 HD44780_SOURCES += hd44780/LCD_ioctl.c hd44780/LCD_nputs.c hd44780/LCD_putc.c
-HD44780_SOURCES += hd44780/LCD_puts.c hd44780/LCD_validate.cZ hd44780/LCD_puts_pgm.c
+HD44780_SOURCES += hd44780/LCD_puts.c hd44780/LCD_validate.c hd44780/LCD_puts_pgm.c
 
 HD44780_OBJECTS = hd44780/LCD_clrline.o hd44780/LCD_gotoxy.o hd44780/LCD_init.o
 HD44780_OBJECTS += hd44780/LCD_ioctl.o hd44780/LCD_nputs.o hd44780/LCD_putc.o
